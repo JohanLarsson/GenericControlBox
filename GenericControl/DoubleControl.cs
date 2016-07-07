94bfff8b801c9d9b0d5f387f63c00249bf10ac0e
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-07 15:07:32 +0200

    DoubleControl

---
+++ GenericControl/DoubleControl.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericControl
+{
+    using System.Windows;
+
+    public class DoubleControl : NumberControl
+    {
+        public new static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(Value),
+            typeof(double),
+            typeof(DoubleControl),
+            new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public new double Value
+        {
+            get { return (double)this.GetValue(ValueProperty); }
+            set { this.SetValue(ValueProperty, value); }
+        }
+    }
+}
--- GenericControlBox/ViewModel.cs
+++ GenericControlBox/ViewModel.cs
@@ -6,6 +6,7 @@
     public class ViewModel : INotifyPropertyChanged
     {
         private int intValue = 2;
+        private double doubleValue = 2.1;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -22,6 +23,21 @@
                 this.OnPropertyChanged();
             }
         }
+
+        public double DoubleValue
+        {
+            get
+            {
+                return this.doubleValue;
+            }
+            set
+            {
+                if (value == this.doubleValue) return;
+                this.doubleValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
