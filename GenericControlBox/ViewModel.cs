ab4d00959829a6ce7ccd1f88a204403ec21d76b3
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-07 14:08:48 +0200

    Broken with binding.

---
+++ GenericControlBox/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace GenericControlBox
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private int intValue = 2;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int IntValue
+        {
+            get
+            {
+                return this.intValue;
+            }
+            set
+            {
+                if (value == this.intValue) return;
+                this.intValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
